d0ddd42c109f79661c6cc42dc1f564ccebcc2d0c
nicolasJott <nicobox2002@gmail.com>
2023-02-07 11:26:54 -0500

    Added dynamic calendar functionality along with styling

--- client/src/utils/formatData.js
+++ client/src/utils/formatData.js
@@ -1,18 +1,19 @@
 
 
-function formatDate(month, day ) {
+export const  formatDate = () => {
     const months = ["January", "February", "March", "April", "May","June", "July", "August", "September", "October", "November", "December"]
     const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday"]
     const date = new Date()
     const monthData = date.getMonth()
     const dayData = date.getDay()
+    let month, day;
     for (let i = 0; i < months.length; i++){
         if (monthData === i) {
             month = months[i]
         }
     }
     for (let i = 0; i < days.length; i++){
-        if (dayData === i) {
+        if (dayData - 1 === i) {
             day = days[i]
         }
     }
@@ -20,4 +21,15 @@ function formatDate(month, day ) {
     return { month, day }
 }
 
-export default formatDate
+export const getDaysInMonth = (month, year) => {
+    let date = new Date(year, month, 1);
+    let days = []
+    while (date.getMonth() === month ){
+        days.push(new Date(date).getDate());
+        date.setDate(date.getDate() + 1)
+    }
+    return days;
+}
+
+
+
