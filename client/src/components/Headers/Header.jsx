c9234aea641533e787004782c319590e6c22ce80
NicolasJott <nicobox2002@gmail.com>
2023-02-13 22:40:22 -0500

    Made styling changes to header

--- client/src/components/Headers/Header.jsx
+++ client/src/components/Headers/Header.jsx
@@ -2,38 +2,76 @@ import {Link } from "react-router-dom";
 import {logout} from "../../actions/auth";
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
+import {faUserCircle, faBars} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import React, {useEffect, useState} from "react";
 
 
+const Header = ({ auth: { isAuthenticated, loading, user }, logout }) => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+    useEffect(() => {
+        const closeMenu = () => {
+            setIsMenuOpen(false);
+        };
 
-const Header = ({ auth: { isAuthenticated, loading, user }, logout}) => {
+        // Close the menu when a link is clicked or when the user clicks outside the menu
+        document.addEventListener("click", closeMenu);
+        return () => {
+            document.removeEventListener("click", closeMenu);
+        };
+    }, []);
+
+    const toggleMenu = (e) => {
+        e.stopPropagation();
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     const authVisible = (
-        <><div className="navMenu">
+        <div className="navMenu">
             <Link to="/">Home</Link>
             <Link to="/workout">Workout</Link>
             <Link to="/meal">Calorie Tracker</Link>
+            <button className="navbtn" onClick={logout}>
+                <FontAwesomeIcon icon={faUserCircle} size="2xl" /> Hello, {user && user.firstName}!
+            </button>
         </div>
-            <div className="rightMenu">
-                <button className="navbtn" onClick={logout}>Hello, {user && user.firstName}!</button>
-            </div></>
+    );
 
+    const authVisibleSmall = (
+        <div className="nav-dropdown">
+            <Link to="/">Home</Link>
+            <Link to="/workout">Workout</Link>
+            <Link to="/meal">Calorie Tracker</Link>
+            <button onClick={logout}>
+                Logout
+            </button>
+        </div>
     )
 
     const guestVisible = (
-        <Link to="/login">Login</Link>
-    )
-
-    return (
-       <header>
-           <div className="logo">
-               <h1><Link to="/">FitnessTrack</Link></h1>
-           </div>
-
-           {!loading && <>{isAuthenticated ? authVisible : guestVisible}</>}
-       </header>
+        <div className="navMenu">
+            <Link to="/login">Login</Link>
+        </div>
     );
 
-}
+    return (
+        <div className="nav-center">
+            <header>
+                <h1 className="logo">FitnessTrack</h1>
+                {isAuthenticated ? authVisible : guestVisible}
+                {isMenuOpen && (
+                    <div className="dropdown">
+                        {isAuthenticated ? authVisibleSmall : guestVisible}
+                    </div>
+                )}
+                <button className="nav-toggle" onClick={toggleMenu}>
+                    <FontAwesomeIcon icon={faBars} />
+                </button>
+            </header>
+        </div>
+    );
+};
 
 Header.propTypes = {
     logout: PropTypes.func.isRequired,
@@ -42,4 +80,4 @@ Header.propTypes = {
 
 const mapStateToProps = (state) => ({ auth: state.auth });
 
-export default connect(mapStateToProps, {logout})(Header);
+export default connect(mapStateToProps, { logout })(Header);
