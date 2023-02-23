5c53b77f3828c9dc2ea00b67800b60ce29121d31
NicolasJott <nicobox2002@gmail.com>
2023-02-22 19:12:28 -0500

    deploy

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const buildPath = path.join(_dirname  , "../client/build");
 
 app.use(express.static(buildPath))
 
-app.get("/*", function(req, res){
+app.get("*", function(req, res){
     console.log("catch all route triggered")
     res.sendFile(
         path.join(__dirname, "../client/build/index.html"),
